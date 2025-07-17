7db3b8f0179859f8f1d0217e6d13ae4ff438eb35
umberto46x <daniele.u.nicosia.dev@gmail.com>
2025-07-17 22:14:26 +0200

    [feat] started working on user-details page

--- src/app/features/user-details/user-details.ts
+++ src/app/features/user-details/user-details.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-details',
   imports: [],
   templateUrl: './user-details.html',
-  styleUrl: './user-details.scss'
+  styleUrl: './user-details.scss',
 })
 export class UserDetails {
+  activatedRoute: ActivatedRoute = inject(ActivatedRoute);
 
+  constructor() {
+    this.activatedRoute.params.subscribe((data) => {
+      console.log(data['userId']);
+    });
+  }
 }
