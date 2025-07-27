6154860f7bd30235a88c9c606da61496329a2173
umberto46x <daniele.u.nicosia.dev@gmail.com>
2025-07-28 00:44:27 +0200

    install,ngrx entity,schematics

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,17 @@
 import {
   ApplicationConfig,
+  isDevMode,
   provideBrowserGlobalErrorListeners,
   provideZoneChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideStore } from '@ngrx/store';
 import { todoReducer } from './state/todos/todos.reducer';
+import { provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -17,5 +20,9 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(),
     provideStore({ todos: todoReducer }),
+    provideEffects([
+      /*  */
+    ]),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
   ],
 };
--- src/app/state/todos/todos.selectors.ts
+++ src/app/state/todos/todos.selectors.ts
@@ -2,9 +2,9 @@ import { createSelector } from '@ngrx/store';
 import { AppState } from '../app.state';
 import { TodoState } from './todos.reducer';
 
-export const selectTodos = (state: AppState) => state.todos;
+export const selectTodoState = (state: AppState) => state.todos;
 
 export const selectAllTodos = createSelector(
-  selectTodos,
+  selectTodoState,
   (state: TodoState) => state.todos
 );
